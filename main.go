27ff920ad3b237d5a672e0c6a1f0328acd6c711d
Toyyib <hamzatbolaji648@yahoo.com>
2025-01-11 11:22:09 +0100

    Authenticate

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	routes "github.com/Simpleshaikh1/golang-jwt/routes"
 	"github.com/gin-gonic/gin"
-	routes "jwt-auth/routes"
 	"os"
 )
 
@@ -23,7 +23,7 @@ func main() {
 		c.JSON(200, gin.H{"success": "Access granted for api-1"})
 	})
 
-	router.Get("/api-2", func(c *gin.Context) {
+	router.GET("/api-2", func(c *gin.Context) {
 		c.JSON(200, gin.H{"success": "Access granted for api-2"})
 	})
 
